5768bae9a5199d3306123054a0981b569ead6251
draichi <lucasdraichi@gmail.com>
2019-05-05 15:48:59 -0300

    updating main

--- main.py
+++ main.py
@@ -8,19 +8,17 @@ from env.BitcoinTradingEnv import BitcoinTradingEnv
 
 import pandas as pd
 
-df = pd.read_csv('./data/bitstamp.csv')
-df = df.sort_values('Timestamp')
+train_df = pd.read_csv('./datasets/bot_train_ETHBTC_700_hour.csv')
+train_df = train_df.sort_values('Date')
 
-slice_point = int(len(df) - 50000)
-
-train_df = df[:slice_point]
-test_df = df[slice_point:]
+test_df = pd.read_csv('./datasets/bot_rollout_ETHBTC_700_hour.csv')
+test_df = test_df.sort_values('Date')
 
 train_env = DummyVecEnv(
     [lambda: BitcoinTradingEnv(train_df, serial=True)])
 
 model = PPO2(MlpPolicy, train_env, verbose=1, tensorboard_log="./tensorboard/")
-model.learn(total_timesteps=200000)
+model.learn(total_timesteps=5000)
 
 test_env = DummyVecEnv(
     [lambda: BitcoinTradingEnv(test_df, serial=True)])
@@ -29,6 +27,6 @@ obs = test_env.reset()
 for i in range(50000):
     action, _states = model.predict(obs)
     obs, rewards, done, info = test_env.step(action)
-    test_env.render(mode="system", title="BTC")
+    test_env.render(mode="human", title="BTC")
 
 test_env.close()
