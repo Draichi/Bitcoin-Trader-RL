0616552af50674cf743d5e94414b153084dd71a3
draichi <lucasdraichi@gmail.com>
2019-05-05 15:48:48 -0300

    adding configs

---
+++ configs/vars.py
@@ -0,0 +1,11 @@
+import datetime
+import os
+import numpy as np
+#---------------------------------------------------------------------------------->
+####### MARKET PARAMETERS: #########
+WALLET_FIRST_SYMBOL = 10
+WALLET_SECOND_SYMBOL = 1
+FEES = 0.002 # 0.2%
+#---------------------------------------------------------------------------------->
+TODAYS_MONTH = datetime.datetime.now().month
+TODAYS_DAY = datetime.datetime.now().day
